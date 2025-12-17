65c92a95445162bc6e4572148a7dca9138f393fe
caio <caiorbcavalcante@gmail.com>
2025-12-17 12:03:59 -0300

    fix: Melhorando o crawler

--- src/crawler.js
+++ src/crawler.js
@@ -2,49 +2,72 @@ const cheerio = require("cheerio");
 const { fetchHtml } = require(".."); // index.js
 const { scrape } = require(".."); // index.js
 
-async function crawler(html) {
-  const $ = cheerio.load(html);
-  const links = [];
+// async function crawler(html) {
+//   const $ = cheerio.load(html);
+//   const links = [];
 
-  $("a").each((_, el) => {
-    if (links.length >= 20) return false;
+//   $("a").each((_, el) => {
+//     if (links.length >= 20) return false;
+
+//     const href = $(el).attr("href");
+//     if (!href) return;
+//     if (!href.includes(".ghtml")) return;
+
+//     const url = href.startsWith("http") ? href : `https://g1.globo.com${href}`;
+
+//     if (!links.includes(url)) {
+//       links.push(url);
+//     }
+//   });
+
+//   return links;
+// }
+
+async function crawler(html, baseUrl, limit = 20) {
+  const $ = cheerio.load(html);
+  const links = new Set(); // evita duplicados automaticamente
+
+  $("a[href]").each((_, el) => {
+    if (links.size >= limit) return false;
 
     const href = $(el).attr("href");
-    if (!href) return;
+
     if (!href.includes(".ghtml")) return;
+    if (
+      href.includes("/videos/") ||
+      href.includes("/infografico") ||
+      href.includes("/ao-vivo")
+    )
+      return;
 
-    const url = href.startsWith("http") ? href : `https://g1.globo.com${href}`;
+    const url = href.startsWith("http") ? href : `${baseUrl}${href}`;
 
-    if (!links.includes(url)) {
-      links.push(url);
-    }
+    links.add(url);
   });
 
-  return links;
+  return [...links];
 }
 
-async function printNews(links){
-    for(const link of links){
-        const html = await fetchHtml(link)
-        scrape(html);
-        console.log("################")
-    }
-
+async function printNews(links) {
+  for (const link of links) {
+    const html = await fetchHtml(link);
+    scrape(html);
+    console.log("################");
+  }
 }
 
 async function main() {
-  const url = "https://g1.globo.com/";
+  const url = "https://g1.globo.com";
 
   try {
-      const html = await fetchHtml(url);
-      if (!html) return;
-    
-      const links = await crawler(html);
-    
-      await printNews(links);
-    
+    const html = await fetchHtml(url);
+    if (!html) return;
+
+    const links = await crawler(html, url);
+
+    await printNews(links);
   } catch (error) {
-    console.log("Ocoreu o erro: ", error)
+    console.log("Ocoreu o erro: ", error);
   }
 }
 
