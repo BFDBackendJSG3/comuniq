5cbdd85180bdf5856f9ee0b098b0dbdc6c6bfa3a
vctr <victoremaanoel@outlook.com>
2025-12-19 00:11:21 -0300

    refactor: config pastas do front e instalando algumas dependencias

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ThemeProvider } from './components/ThemeProvider.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
