b68acd1ceb96cbb478d562edda750a97119ad96a
vctr <victoremaanoel@outlook.com>
2025-12-22 23:13:12 -0300

    chore: add shadcn css e botão

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import  tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tailwindcss from "@tailwindcss/vite"
+import { fileURLToPath } from "url"
+import path from "path"
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
