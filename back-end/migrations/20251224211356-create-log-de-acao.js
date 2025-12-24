39795dba5ff2ab75f34f2cc1ca0c6dbed0507943
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2025-12-24 18:19:11 -0300

    Feat: adiciona migration e model para a entidade LogAcoes

---
+++ back-end/migrations/20251224211356-create-log-de-acao.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('LogDeAcoes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      acao: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      entidade: {
+        type: Sequelize.STRING
+      },
+      entidadeId: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      descricao: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addIndex('LogDeAcoes', ['userId']);
+    await queryInterface.addIndex('LogDeAcoes', ['entidade']);
+    await queryInterface.addIndex('LogDeAcoes', ['entidadeId']);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('LogDeAcoes');
+  }
+};
---
+++ back-end/models/logdeacao.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class LogDeAcao extends Model {
+    static associate(models) {
+      LogDeAcao.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'usuario'
+      });
+    }
+  }
+  LogDeAcao.init({
+    userId: DataTypes.INTEGER,
+    acao: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entidade: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entidadeId: DataTypes.INTEGER,
+    descricao: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'LogDeAcao',
+    tableName: 'LogDeAcoes'
+  });
+
+  return LogDeAcao;
+};
