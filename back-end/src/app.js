b33edfa22280203aefa7537b802efa137b357d73
caio <caiorbcavalcante@gmail.com>
2025-12-27 12:54:13 -0300

    feat: CRUD de usuário feito

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,8 +5,10 @@ app.use(express.json());
 
 const importacaoRoutes = require('./routes/importacao.routes');
 const noticiasRoutes = require('./routes/noticias.routes');
+const userRoutes = require('./routes/user.routes');
 
 app.use(importacaoRoutes);
 app.use(noticiasRoutes);
+app.use('/users', userRoutes);
 
 module.exports = app;
---
+++ back-end/src/routes/user.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const UserController = require('../controllers/UserController');
+
+const router = Router();
+
+router.get('/', UserController.listar);
+router.get('/:id', UserController.buscar);
+router.post('/', UserController.criar);
+router.put('/:id', UserController.atualizar);
+router.delete('/:id', UserController.deletar);
+
+module.exports = router;
